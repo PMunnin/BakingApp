78db00b662c79e5487c742e995fe5b961a192617
Pedro Martín <pmunnin@gmail.com>
2018-11-22 11:35:42 +0100

    Created Espresso test for recipeDetaisActivity

---
+++ app/src/androidTest/java/com/example/android/bakingapp/ActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.android.bakingapp;
+
+import android.os.SystemClock;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class ActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> recipeDetailsActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void checkRecipeSteps(){
+        SystemClock.sleep(1000);
+
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions
+                .actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.recipe_name_title)).check(matches(withText("Nutella Pie")));
+        
+        onView(withId(R.id.recipe_steps_recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+    }
+}
